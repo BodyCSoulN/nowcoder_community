3e381dbfd6f8ad63659b8a18c61b1c56fc86d8ec
BodyCSoulN <1264968325@qq.com>
2023-02-08 21:53:36 +0800

    Initial commit

---
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -0,0 +1,37 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.AlphaDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Service
+// bean默认是单例的 @Scope("Singleton")
+// 多例 @Scope("prototype")
+public class AlphaService {
+
+    @Autowired
+    private AlphaDao alphaDao;
+
+    public AlphaService(){
+        System.out.println("实例化AlphaService");
+    }
+
+    @PostConstruct
+    // 初始化方法在构造器之后调用
+    public void init(){
+        System.out.println("初始化AlphaService");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("销毁AlphaService");
+    }
+
+    public String find(){
+        return alphaDao.select();
+    }
+}
