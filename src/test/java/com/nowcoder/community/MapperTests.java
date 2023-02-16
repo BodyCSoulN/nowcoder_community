c8504f5fe65db6f6a0141248cacc325ac43f06c0
BodyCSoulN <1264968325@qq.com>
2023-02-16 15:55:29 +0800

    完成用户设置开发，支持进行头像更新; 完成登陆状态检查，防止出现恶意访问；

---
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -0,0 +1,79 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
+import com.nowcoder.community.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Date;
+import java.util.List;
+
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
+
+    @Test
+    public void testSelectUser(){
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+
+        user = userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        user = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+
+    }
+
+    @Test
+    public void testSelectPosts(){
+        System.out.println("这是中文");
+        List<DiscussPost> list = discussPostMapper.selectDisscussPosts(149, 0, 10);
+        for (DiscussPost post : list){
+            System.out.println(post);
+        }
+
+        int rows = discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(rows);
+    }
+
+    @Test
+    public void testInsertLoginTicket() {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setId(101);
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectByLoginTicket() {
+        String ticket = "abc";
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket(ticket);
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus(ticket, 1);
+        LoginTicket loginTicket1 = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket1);
+
+    }
+
+}
